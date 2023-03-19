25f82588be87f2a490ef0538043c14fd81d6d724
csriramp <csriramprasad@gmail.com>
2023-03-19 19:57:04 +0530

    Prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -51,3 +51,12 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
         return report
     except Exception as e:
         raise CustomException(e,sys)
+
+# This is for loading the model and used in predict_pipeline.py of src/pipeline folder    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
